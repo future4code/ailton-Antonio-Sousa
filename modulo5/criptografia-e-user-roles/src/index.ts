9fb233f6849a38dbdb9ddcd22f68babbbd7210cf
CarlosSousa2001 <carlosramos10k@gmail.com>
2022-09-13 17:15:06 -0300

    usando compare

--- modulo5/criptografia-e-user-roles/src/index.ts
+++ modulo5/criptografia-e-user-roles/src/index.ts
@@ -2,12 +2,24 @@ import * as bcrypts from 'bcryptjs'
 import { app } from './app'
 
 // Exercício 1
+app.get('/verSenha', async (req, res)=> {
 
-// A) round:       --  salt:Cadeira de caracteres aleatórios
-async function  hashDaSenha(senha:string){
-  const cost = 12
-  const salt = await bcrypts.genSalt(cost)
-  const hash = await bcrypts.hash(senha, salt)
+  async function  hashDaSenha(senha:string){
+    const cost = 12
+    const salt = await bcrypts.genSalt(cost)
+    const hash = await bcrypts.hash(senha, salt)
+    return hash
+  }
+  async function compare(senha:string, hash:string){
+    return  bcrypts.compare(senha, hash)
+
+  }
+  const hash = await hashDaSenha('Carlos2001')
   console.log(hash)
-}
-hashDaSenha('Carlos2001')
+  const senhaComparada = await compare("Carlos2001",hash)
+  console.log(senhaComparada)
+})
+// A) round:       --  salt:Cadeira de caracteres aleatórios
+
+
+
